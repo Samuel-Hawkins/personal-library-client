eca520e33906a24a23ca6bbff556b5ed288b7fe6
samuel-hawkins <samueledwardhawkins@gmail.com>
2020-09-22 11:47:04 -0400

    Updated scripts, CSS and HTML
    - updated scripts to show/hide when it is appropriate
    - updated CSS and HTML to make website cleaner

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,16 +2,6 @@
 
 const store = require('./../store')
 
-const onSignInButton = function () {
-  $('#sign-in-form').show()
-  $('#sign-up-form').hide()
-}
-
-const onSignUpButton = function () {
-  $('#sign-up-form').show()
-  $('#sign-in-form').hide()
-}
-
 const changePassForm = function () {
   $('#pass-change-form').show()
 }
@@ -20,6 +10,8 @@ const onSignUpSuccess = function (response) {
   $('#message').text('Thanks for signing up ' + response.user.email)
   $('#sign-up-form').trigger('reset')
   $('#sign-up-form').hide()
+  $('#signInButton').show()
+  $('#signUpButton').show()
 }
 
 const onSignUpFailure = function () {
@@ -29,12 +21,14 @@ const onSignUpFailure = function () {
 const onSignInSuccess = function (response) {
   store.user = response.user
 
-  $('#user-name').text('Welcome ' + response.user.email)
+  $('#message').text('Welcome ' + response.user.email)
   $('#sign-in-form').trigger('reset')
   $('#sign-in-form').hide()
   $('#sign-up-form').hide()
+  $('#signInButton').hide()
+  $('#signUpButton').hide()
   $('#signOut').show()
-  $('#pass-change-form').show()
+  $('#pass-change-button').show()
   $('#getBooks').show()
   $('#book-create').show()
   $('#signOut').show()
@@ -49,7 +43,7 @@ const onPassChangeSuccess = function (response) {
   $('#message').show()
   $('#message').text('Successful Password Reset')
   $('#pass-change-form').trigger('reset')
-  // $('#pass-change-form').hide()
+  $('#pass-change-form').hide()
 }
 
 const onPassChangeFailure = function () {
@@ -61,12 +55,13 @@ const onSignOutSuccess = function (response) {
   $('#message').text('Successfully Signed Out')
   $('#change').hide()
   $('#signOut').hide()
-  $('#sign-up-form').show()
-  $('#sign-in-form').show()
   $('#pass-change-form').hide()
+  $('#pass-change-button').hide()
   $('#getBooks').hide()
   $('#book-create').hide()
   $('#allMyBooks').html('')
+  $('#signInButton').show()
+  $('#signUpButton').show()
 }
 
 const onSignOutFailure = function () {
@@ -83,7 +78,5 @@ module.exports = {
   onPassChangeFailure: onPassChangeFailure,
   onSignOutSuccess: onSignOutSuccess,
   onSignOutFailure: onSignOutFailure,
-  onSignInButton: onSignInButton,
-  onSignUpButton: onSignUpButton,
   changePassForm: changePassForm
 }
--- assets/scripts/library/ui.js
+++ assets/scripts/library/ui.js
@@ -2,6 +2,9 @@
 
 const showBooksSuccess = function (response) {
   $('#allMyBooks').html('')
+  if (response.books.length === 0) {
+    $('#allMyBooks').append(`<p>You don't seem to have any books, why dont you add some</p>`)
+  }
   for (let i = 0; i < response.books.length; i++) {
     $('#allMyBooks').append(
       `<h4>Book ${i + 1}</h4>
@@ -38,12 +41,28 @@ const updateShow = function () {
   const updateForm = $(event.target).attr('data-value-index')
   $('#' + updateForm).show()
   $('.' + updateForm).hide()
-  $('.' + updateForm).hide()
+}
+const showSignInForm = function () {
+  $('#signInButton').hide()
+  // $('#signUpButton').hide()
+  $('#sign-in-form').show()
+}
+const showSignUpForm = function () {
+  $('#sign-up-form').show()
+  $('#signUpButton').hide()
+  // $('#signInButton').hide()
+}
+
+const showPassChangeForm = function () {
+  $('#pass-change-form').show()
 }
 
 module.exports = {
   showBooksSuccess: showBooksSuccess,
   createNewBookSuccess: createNewBookSuccess,
   deleteBookSuccess: deleteBookSuccess,
-  updateShow: updateShow
+  updateShow: updateShow,
+  showSignInForm: showSignInForm,
+  showSignUpForm: showSignUpForm,
+  showPassChangeForm: showPassChangeForm
 }
